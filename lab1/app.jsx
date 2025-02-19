6f9986f20ba5d70d827b977b6d03e8c3315723f3
ChiorgloNichita <145658422+ChiorgloNichita@users.noreply.github.com>
2025-02-20 01:24:44 +0200

    app.jsx

---
+++ lab1/app.jsx
@@ -0,0 +1,18 @@
+app.jsx
+   ```jsx
+   import Header from "./components/Header.jsx";
+   import ArticleList from "./components/ArticleList.jsx";
+   import Footer from "./components/Footer.jsx";
+
+   function App() {
+     return (
+       <div>
+         <Header />
+         <ArticleList />
+         <Footer />
+       </div>
+     );
+   }
+   
+   export default App;
+   ```
