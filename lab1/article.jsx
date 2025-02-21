349631e820517f0ebb2e40023d29541f51be457a
ChiorgloNichita <145658422+ChiorgloNichita@users.noreply.github.com>
2025-02-21 08:50:33 +0200

    article.jsx

--- lab1/article.jsx
+++ lab1/article.jsx
@@ -1,10 +1,10 @@
-Article.jsx
-   ```jsx
-   function Article({ title, text }) {
-     return (
-       <article>
-         <h2>{title}</h2>
-         <p>{text}</p>
-       </article>
-     );
+```jsx
+export default function Article({title, text}) {
+    return (
+      <article>
+        <h2>{title}</h2>
+        <p>{text}</p>
+      </article>
+    );
    }
+```
